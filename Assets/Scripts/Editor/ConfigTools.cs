9393a2bfaa3bc049b3d77c62e7c9862c67993a21
Vladimir Yanovsky <horaxlfa@gmail.com>
2018-03-30 18:27:38 +0300

    Working on config

---
+++ Assets/Scripts/Editor/ConfigTools.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using DuckOfDoom.Danmaku.Configuration;
+using UnityEditor;
+using UnityEngine;
+
+namespace DuckOfDoom.Danmaku.Editor
+{
+    public class ConfigTools : UnityEditor.Editor
+    {
+        [MenuItem("Tools/Generate Sample Config")]
+        public static void GenerateSampleConfig()
+        {
+            var events = new List<GameplayEvent>();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i % 30 == 0)
+                {
+                    events.Add(new DebugEvent(i, string.Format("this is debug event on {0}", i)));
+                }
+                else if (i % 50 == 0)
+                {
+                    events.Add(new SpawnEvent(i, new Vector2(i, -i)));
+                }
+            }
+                
+                
+            GameplayConfig.GenerateSample(events);
+        }
+        
+    }
+}
